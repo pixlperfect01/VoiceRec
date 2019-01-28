fe6ced225f195269d697daddae031a49786c0a54
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-28 15:36:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+let speech;
+var responses = {
+  
+};
+
 function setup(){
+  noCanvas();
+  let lang = navigator.language || 'en-US';
+  let speechRec = new p5.SpeechRec(lang, gotSpeech);
+  let continuous = true;
+  let interim = true;
+  speechRec.start(continuous, interim);
+  speech = new p5.Speech();
+  speech.onLoad = voiceReady;
+  speech.started(startSpeaking);
+  speech.ended(endSpeaking);
+}
+function gotSpeech() {
+  if (speechRec.resultValue) {
+    reply(speechRec.resultString);
+  }
+}
+function reply(a){
+  speech.speak(findReply(a));
+}
+function findReply(a){
+  
+}
